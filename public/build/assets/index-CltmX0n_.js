44dec51a2091b79de3a347e18b71f1537ccdc519
Dion <beppieoostkust@gmail.com>
2025-12-15 21:26:30 +0100

    Refactor code structure for improved readability and maintainability

--- public/build/assets/index-CltmX0n_.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:public/build/assets/index-BkbQX9qD.js
-import{r as u}from"./app-TjlK6pwN.js";function o(r){const e=u.useRef({value:r,previous:r});return u.useMemo(()=>(e.current.value!==r&&(e.current.previous=e.current.value,e.current.value=r),e.current.previous),[r])}export{o as u};
-========
-import{r as u}from"./app-5rcNGoiL.js";function o(r){const e=u.useRef({value:r,previous:r});return u.useMemo(()=>(e.current.value!==r&&(e.current.previous=e.current.value,e.current.value=r),e.current.previous),[r])}export{o as u};
->>>>>>>> d5b262184001e37ce098600b311f947954a1b753:public/build/assets/index-CltmX0n_.js
--- public/build/assets/text-link-BbGmhMfM.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:public/build/assets/text-link-D4HWdAoj.js
-import{j as t,$ as n}from"./app-TjlK6pwN.js";import{a}from"./button-ZLXudFdJ.js";function u({className:r="",children:e,...o}){return t.jsx(n,{className:a("text-foreground underline decoration-neutral-300 underline-offset-4 transition-colors duration-300 ease-out hover:decoration-current! dark:decoration-neutral-500",r),...o,children:e})}export{u as T};
-========
-import{j as t,$ as n}from"./app-5rcNGoiL.js";import{a}from"./button-DXte1vCw.js";function u({className:r="",children:e,...o}){return t.jsx(n,{className:a("text-foreground underline decoration-neutral-300 underline-offset-4 transition-colors duration-300 ease-out hover:decoration-current! dark:decoration-neutral-500",r),...o,children:e})}export{u as T};
->>>>>>>> d5b262184001e37ce098600b311f947954a1b753:public/build/assets/text-link-BbGmhMfM.js
